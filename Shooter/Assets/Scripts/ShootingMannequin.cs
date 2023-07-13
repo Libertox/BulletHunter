af71066f4f07a3a2886f3dadb6b06195b77b7c58
Libertox <damians34@interia.pl>
2023-07-13 15:16:48 +0200

    Added Shooting Mannequin

---
+++ Shooter/Assets/Scripts/ShootingMannequin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Shooter
+{
+    public class ShootingMannequin:MonoBehaviour, IDamageable
+    {
+
+        private const string ANIM_IS_SHOOT = "isShoot";
+        private Animator animator;
+        private WaitForSeconds waitForSeconds;
+
+        [SerializeField] private float uprightingCooldown;
+      
+        private void Awake()
+        {
+            animator = GetComponent<Animator>();
+            waitForSeconds = new WaitForSeconds(uprightingCooldown);
+        }
+
+        public void TakeDamage()
+        {
+            animator.SetBool(ANIM_IS_SHOOT, true);
+            StartCoroutine(Uprighting());
+        }
+
+        private IEnumerator Uprighting()
+        {
+            yield return waitForSeconds;
+            animator.SetBool(ANIM_IS_SHOOT, false);
+        }
+
+
+
+    }
+}
